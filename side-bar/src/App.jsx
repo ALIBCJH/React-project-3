f52833699ddd34179f353bd755457d21b7448f30
simonjuma_230 <simon.ombom22@students.dkut.ac.ke>
2024-03-28 22:42:04 +0300

    first commit

---
+++ side-bar/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import {Layout} from 'antd';
+import logo from './components/Logo';
+
+const {Header, Sider} = Layout;
+
+function App() {
+  
+
+  return (
+    
+     //<Layout>
+     // <Sider className="sidebar">
+        //<Logo />
+      //</Sider>
+
+    // </Layout>
+<div className="container">
+<h1>Hello Juma this  is your First react App</h1>
+<button className="container2">claim</button>
+<h2>Welcome Home</h2>
+</div>
+    
+    
+  )
+}
+
+export default App;
